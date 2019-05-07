e958ada32f7e9f23651c1082f4e153af26d36579
Sheikh Sayed <sksayed@live.com>
2019-05-07 12:38:22 +0600

    All done by creating homepage

--- Granite House/Data/ApplicationDbContext.cs
+++ Granite House/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Granite_House.Models;
 
 namespace Granite_House.Data
 {
@@ -12,5 +13,7 @@ namespace Granite_House.Data
             : base(options)
         {
         }
+
+        DbSet<ProductTypes> ProductTypes { get; set; }
     }
 }
---
+++ Granite House/Models/ProductTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Granite_House.Models
+{
+    public class ProductTypes
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
